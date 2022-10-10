fa84bd9644d0b11f6ed7f45cdbe99ce65197fb25
Harsh-bhai <107921110+Harsh-bhai@users.noreply.github.com>
2022-10-11 00:11:41 +0530

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <div className="cursor-pointer h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
           <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
           <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Raclette Blueberry Nextious Level</h1>
-          <p className="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat..</p>
+          <p className="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat...</p>
           <a className="text-green-700 inline-flex items-center">Learn More
             <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
