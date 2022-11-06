4435ec67bb2db8e1ffa2b5a83ab994ddcd322ca9
Harsh-bhai <107921110+Harsh-bhai@users.noreply.github.com>
2022-11-06 16:11:41 +0530

    kafi sare update

--- frontend/pages/orders.js
+++ frontend/pages/orders.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Orders = () => {
   return (
-    <div>Orders</div>
+    <div className='flex flex-col space-y-20 justify-center items-center my-20 text-4xl font-bold'><div>No Orders Till Now</div>
+    <button className=" text-white bg-green-600 mx-8  py-2 px-6 focus:outline-none hover:bg-green-700 rounded-full text-lg"> Shop Now</button></div>
   )
 }
 
