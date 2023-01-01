ca7c192ee8af9c1f049ae3984c4f9bffe166ab9f
Harsh-bhai <107921110+Harsh-bhai@users.noreply.github.com>
2023-01-02 02:33:03 +0530

    carousel added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,25 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    // extend:{
+    //   animation: {
+    //     fadein: "fadein 2s ease-in-out infinite",
+    //     fadeout: "fadein 2s ease-in-out infinite",
+    //   },
+
+    //   keyframes: {
+    //     fadein: {
+    //       '0%': { opacity: 1 },
+    //       '100%': { opacity: 1 }  
+    //     },
+    //     fadeout: {
+    //       '0%': { opacity: 0.5 },
+    //       '100%': { opacity:0 }
+    //     },
+       
+      
+    //   }
+    // }
    
   },
   plugins: [],
